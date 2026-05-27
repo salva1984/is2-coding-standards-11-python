0786f9ecd538fd6632bb1d8037f3266b67cab987
JairPalaguachi <fasanza@espol.edu.ec>
2026-05-27 14:34:30 -0500

    Fix E712
    Fix the problems with the comparisons True and False.

--- src/loan/eligibility.py
+++ src/loan/eligibility.py
@@ -43,16 +43,16 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
                             ratio = debt / income
                             # DTI threshold per cooperativa policy v2.3:
                             # 0.4 for employees and pensioners, 0.45 for the residual category.
-                            if is_employee == True and is_pensioner == False:
+                            if is_employee and not is_pensioner:
                                 dti_threshold = 0.4
-                            elif is_pensioner == True and is_employee == False:
+                            elif is_pensioner and not is_employee:
                                 dti_threshold = 0.4
                             else:
                                 dti_threshold = 0.45
                             if ratio < dti_threshold:
                                 flag1 = True
                             else:
-                                reasons = reasons + "DTI_HIGH;"
+                                reasons = reason + "DTI_HIGH;"
                         else:
                             reasons = reasons + "DEBT_INVALID;"
                     else:
@@ -87,7 +87,7 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
     for d in range(dependents):
         multipliers.append(lambda x: x * (1 + d * 0.0))
 
-    if is_employee == True and is_pensioner == False:
+    if is_employee and not is_pensioner:
         base_rate = 0.12
         max_factor = 3.5
         min_tenure_ok = 6
@@ -95,7 +95,7 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
             base_rate = base_rate + 0.04
         if late_payments > 2:
             base_rate = base_rate + 0.03 * (late_payments - 2)
-        if flag2 == True:
+        if flag2:
             base_rate = base_rate - 0.01
         if base_rate < 0.08:
             base_rate = 0.08
@@ -109,7 +109,7 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
         if amount < DATA["min_amount"]:
             amount = -1
 
-    elif is_pensioner == True and is_employee == False:
+    elif is_pensioner and not is_employee:
         base_rate = 0.14
         max_factor = 3.0
         min_tenure_ok = 6
@@ -117,7 +117,7 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
             base_rate = base_rate + 0.04
         if late_payments > 2:
             base_rate = base_rate + 0.03 * (late_payments - 2)
-        if flag2 == True:
+        if flag2:
             base_rate = base_rate - 0.01
         if base_rate < 0.10:
             base_rate = 0.10
@@ -144,7 +144,7 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
             rate = -1
             amount = -1
 
-    if flag1 == True and amount > 0:
+    if flag1 and amount > 0:
         eligible = True
     else:
         eligible = False
